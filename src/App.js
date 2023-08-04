0efb9d7a7190e0378391900ec5373c5df99bf9d3
Dominick Spinetti <spinettidominick@gmail.com>
2023-08-04 13:54:21 -0700

    Adjusting front page and adding the add-todo button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
+
+
+const MyButton = () => {
+  return (
+    <button className="custom-button">Add to-do</button>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className = "App-header-text">To-Do Application</header>
+      <MyButton/>
       </header>
+        <p>
+          
+        </p>
+        
     </div>
   );
 }
